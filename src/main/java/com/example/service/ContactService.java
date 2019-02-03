0bafcc277f3becdef516d364204e8d7ec83e5e84
Ilinka Poletan <ilinka.p97@gmail.com>
2019-02-03 14:36:59 +0100

    contacts service added

---
+++ src/main/java/com/example/service/ContactService.java
@@ -0,0 +1,32 @@
+package com.example.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entities.Contact;
+import com.example.repository.ContactRepository;
+
+@Service
+public class ContactService {
+
+	private ContactRepository contactRepo;
+	
+	@Autowired
+	public ContactService(ContactRepository contactRepo) {
+		this.contactRepo=contactRepo;
+	}
+	
+	public List<Contact> findAllContacts() {
+		Iterable<Contact> contactsDAO=contactRepo.findAll();
+		List<Contact> contacts=new ArrayList<>();
+		
+		for (Contact contact : contactsDAO) {
+			contacts.add(contact);
+		}
+		return contacts;
+		
+	}
+}
