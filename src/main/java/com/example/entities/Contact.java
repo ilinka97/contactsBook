b9247884df0cb6b9fc6d8f9927701b2d0928ff8b
Ilinka Poletan <ilinka.p97@gmail.com>
2019-02-04 22:45:18 +0100

    removed h2 database from classpath, added mysql and required configuration, created enum for group and annotated it properly in entity class

--- src/main/java/com/example/entities/Contact.java
+++ src/main/java/com/example/entities/Contact.java
@@ -1,29 +1,32 @@
 package com.example.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @Entity
-@NoArgsConstructor
 public class Contact {
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long contactId;
+	
 	private String contactName;
 	private String phoneNumber;
 	private String email;
-	private Group group;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(length = 15)
+	private Group group; 
+	
 	private String address;
 	
-	public static enum Group{
-		FAMILY,WORK,FRIENDS
-	}
 	
 }
---
+++ src/main/java/com/example/entities/Group.java
@@ -0,0 +1,5 @@
+package com.example.entities;
+
+public enum Group {
+	FAMILY,WORK,FRIENDS
+}
