331f3847269e111a9ad77acef7c62c5e7dfdfb21
Ilinka Poletan <ilinka.p97@gmail.com>
2019-02-07 16:45:32 +0100

    added home controller

---
+++ src/main/java/com/example/controller/HomeController.java
@@ -0,0 +1,38 @@
+package com.example.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.entities.Contact;
+import com.example.service.ContactService;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+	private ContactService contactService;
+	
+	@Autowired
+	public HomeController(ContactService contactService) {
+		this.contactService=contactService;
+	}
+	
+	@GetMapping
+	public String showAllContacts(Model model) {
+		
+		List<Contact> contacts=contactService.findAllContacts();
+		
+		for (Contact contact : contacts) {
+			model.addAttribute(contact);
+		}
+		
+		return "home";
+	}
+	
+	
+}
