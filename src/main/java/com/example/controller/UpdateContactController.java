5458d2a6f9d181e0e10ca252416405132a30b3f9
Ilinka Poletan <ilinka.p97@gmail.com>
2019-02-19 23:40:31 +0100

    added validation when updating contact

--- src/main/java/com/example/controller/UpdateContactController.java
+++ src/main/java/com/example/controller/UpdateContactController.java
@@ -1,8 +1,11 @@
 package com.example.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,7 +33,10 @@ public class UpdateContactController {
 	}
 	
 	@PostMapping
-	public String submitUpdateForm(Contact contact) {
+	public String submitUpdateForm(@Valid Contact contact, Errors errors) {
+		if (errors.hasErrors()) {
+			return "editContact";
+		}
 		contactService.saveContact(contact);
 		return "redirect:/home";
 	}
