284e067d6f6d900a41394dba8e665932c397dbff
Ilinka Poletan <ilinka.p97@gmail.com>
2019-03-12 21:24:17 +0100

    default contact photo gets displayed in contacts table, contact photo not jet

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -1,18 +1,26 @@
 package com.example.controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.entities.Contact;
 import com.example.entities.User;
+import com.example.service.ContactPhotoService;
 import com.example.service.ContactService;
 
 @Controller
@@ -20,11 +28,33 @@ import com.example.service.ContactService;
 public class HomeController {
 
 	private ContactService contactService;
+	private ContactPhotoService photoService;
 	
 	@Autowired
-	public HomeController(ContactService contactService) {
+	public HomeController(ContactService contactService, ContactPhotoService photoService) {
 		this.contactService=contactService;
+		this.photoService=photoService;
 	}
+	
+	@GetMapping("/{filename}")
+	@ResponseBody
+	public ResponseEntity<?> showOnePhoto(@PathVariable String filename){
+		
+		try {
+			
+			Resource file=photoService.findOnePhoto(filename);
+			return ResponseEntity.ok()
+					.contentLength(file.contentLength())
+					.contentType(MediaType.IMAGE_JPEG)
+					.body(new InputStreamResource(file.getInputStream()));
+			
+		} catch (IOException e) {
+			return ResponseEntity.badRequest()
+					.body("Couldn't find "+filename+e.getMessage());
+		}
+		
+	}
+	
 	
 	@GetMapping
 	public String showAllContacts(Model model, @AuthenticationPrincipal User user) {
--- src/main/java/com/example/service/ContactPhotoService.java
+++ src/main/java/com/example/service/ContactPhotoService.java
@@ -5,6 +5,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -12,6 +15,13 @@ import org.springframework.web.multipart.MultipartFile;
 public class ContactPhotoService {
 
 	private static String UPLOADED_FOLDER = "C:\\Users\\Danijel&Ika\\Documents\\contact-book-images\\";
+	
+	private ResourceLoader resourceLoader;
+	
+	@Autowired
+	public ContactPhotoService(ResourceLoader resourceLoader){
+		this.resourceLoader=resourceLoader;
+	}
 	
 	public String savePhoto(MultipartFile file) {
 		
@@ -29,4 +39,7 @@ public class ContactPhotoService {
 		return absolutePath;
 	}
 	
+	public Resource findOnePhoto(String photoPath) {
+		return resourceLoader.getResource(photoPath);
+	}
 }
