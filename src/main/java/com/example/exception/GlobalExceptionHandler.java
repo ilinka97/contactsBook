4f9b232ba6bfbee5c2ea2aa5b23c4d28278d1cda
Ilinka Poletan <ilinka.p97@gmail.com>
2019-03-15 12:41:24 +0100

    set the max upload size of the file

---
+++ src/main/java/com/example/exception/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.example.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.multipart.MultipartException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MultipartException.class)
+    public String handleError1(MultipartException e, RedirectAttributes redirectAttributes) {
+
+        redirectAttributes.addFlashAttribute("flash", "Failed to upload. File size to large!");
+        return "redirect:/saveContact";
+
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public String handleError2(MaxUploadSizeExceededException e, RedirectAttributes redirectAttributes) {
+
+        redirectAttributes.addFlashAttribute("flash", "Failed to upload. File size to large!");
+        return "redirect:/saveContact";
+
+    }
+}
